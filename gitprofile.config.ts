89dd67a98efec71f7d4dae4d604eddd00fb2bf0c
Ivan Gaponenko <145093770+igaponenko@users.noreply.github.com>
2024-10-02 23:32:06 +0400

    Update gitprofile.config.ts

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -65,7 +65,7 @@ const CONFIG = {
   },
   resume: {
     fileUrl:
-      'https://archive.org/details/ivan-gaponenko-cv', // Empty fileUrl will hide the `Download Resume` button.
+      'https://archive.org/details/ivan-gaponenko-cv_202410', // Empty fileUrl will hide the `Download Resume` button.
   },
   skills: [
     'JavaScript',
